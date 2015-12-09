00324e483970172b682616a4549976c84409cf2a
Sandybox <sandybox@pleaseignore.com>
2015-12-09 12:00:09 -0600

    Add fix for _get_content issue

--- piston/utils.py
+++ piston/utils.py
@@ -52,12 +52,42 @@ class rc_factory(object):
         except TypeError:
             raise AttributeError(attr)
 
+        #class HttpResponseWrapper(HttpResponse):
+        #    """
+        #    Wrap HttpResponse and make sure that the internal
+        #    _is_string/_base_content_is_iter flag is updated when the
+        #    _set_content method (via the content property) is called
+        #    """
+        #    def _set_content(self, content):
+        #        """
+        #        Set the _container and _is_string /
+        #        _base_content_is_iter properties based on the type of
+        #        the value parameter. This logic is in the construtor
+        #        for HttpResponse, but doesn't get repeated when
+        #        setting HttpResponse.content although this bug report
+        #        (feature request) suggests that it should:
+        #        http://code.djangoproject.com/ticket/9403
+        #        """
+        #        is_string = False
+        #        if not isinstance(content, basestring) and hasattr(content, '__iter__'):
+        #            self._container = content
+        #        else:
+        #            self._container = [content]
+        #            is_string = True
+        #        if django.VERSION >= (1, 4):
+        #            self._base_content_is_iter = not is_string
+        #        else:
+        #            self._is_string = is_string
+
+        #    content = property(HttpResponse._get_content, _set_content)
+        
         class HttpResponseWrapper(HttpResponse):
             """
             Wrap HttpResponse and make sure that the internal
             _is_string/_base_content_is_iter flag is updated when the
             _set_content method (via the content property) is called
             """
+
             def _set_content(self, content):
                 """
                 Set the _container and _is_string /
@@ -79,8 +109,18 @@ class rc_factory(object):
                 else:
                     self._is_string = is_string
 
-            content = property(HttpResponse._get_content, _set_content)            
+            try:
+                # Django versoin is older than 1.5
 
+                content = property(HttpResponse._get_content, _set_content)
+
+            except:
+                # Django version 1.5
+
+                @HttpResponse.content.setter
+                def content(self, content):
+                    self._set_content(content)
+                
         return HttpResponseWrapper(r, content_type='text/plain', status=c)
     
 rc = rc_factory()
